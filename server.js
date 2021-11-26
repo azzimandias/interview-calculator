df7ced2cc653bc4d61454eefef78448c7f3d1473
Andrew Sharanda <azzimandias@gmail.com>
2021-11-26 05:48:01 +0300

    build for heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use('/interview-calculator/', serveStatic(path.join(__dirname, '/dist')));
 
 app.get(/.*/,  (req, res) => {
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 const port = process.env.PORT || 8080;
