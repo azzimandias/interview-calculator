4feb6a3f785de2b5fac9554e834542999b416a17
Andrew Sharanda <azzimandias@gmail.com>
2022-04-08 11:09:14 +0300

    Delete unused styles

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue'
 import store from './store'
 import './styles/normalize.css'
 import './styles/main.css'
-import './styles/light.css'
-import './styles/dark.css'
 import './styles/personalColorTheme.css'
 
 Vue.config.productionTip = false
